a0a2e81e25f703cec04386859e98e66efb95c964
Ruslan Gilmanov <r_gilmanov2005@hotmail.com>
2024-08-14 14:44:57 +1000

    Added unit tests for show_exchange_rate(), convert_value() and check_currency_codes() functions

--- tests/test_other_funcs.py
+++ tests/test_other_funcs.py
@@ -1,14 +1,59 @@
-from currency_converter import output_all_currencies
+import pytest
+from currency_converter import (output_all_currencies,
+                                show_exchange_rate,
+                                convert_value,
+                                check_currency_codes)
 
 
+lst_currencies = ['USD', 'EUR', 'JPY', 'GBP', 'AUD']
 
 api_data = {'data': {'AUD': 1.518340182,
-                     'BGN': 1.787220253,
-                     'BRL': 5.4922310083
+                     'USD': 1,
+                     'EUR': 0.9141001224,
+                     'GBP': 0.7829601241,
+                     'JPY': 146.9292393414,
                     }
             }
 
 
 def test_output_all_currencies():
-    output_str = 'AUD - Australian Dollar\nBGN - Bulgarian Lev\nBRL - Brazilian Real'
+    output_str = 'AUD - Australian Dollar\nUSD - United States Dollar\nEUR - Euro\nGBP - British Pound Sterling\nJPY - Japanese Yen'
     assert output_all_currencies(api_data) == output_str
+
+
+def test_show_exchange_rate():
+    assert show_exchange_rate(api_data, "EUR") == 0.9141001224
+
+
+def test_convert_value_valid():
+    conversion_lst = ['100', 'USD', 'EUR']
+    assert convert_value(api_data, conversion_lst, lst_currencies) == 91.41001224
+
+
+@pytest.mark.parametrize('conv_lst, expected_error', [(['0', 'USD', 'EUR'], ValueError),
+                                                      (['-10', 'USD', 'EUR'], ValueError),
+                                                      (['-10', 'USD', 'USD'], ValueError),
+                                                      (['-10', 'USD', 'KZT'], ValueError),
+                                                      (['-10', 'RUB', 'KZT'], ValueError),
+                                                      (['Hello', 'USD', 'AUD'], ValueError),
+
+])
+def test_convert_value_invalid(conv_lst, expected_error):
+    with pytest.raises(expected_error):
+        convert_value(api_data, conv_lst, lst_currencies)
+
+
+def test_check_currency_codes_valid():
+    conversion_lst = ['100', 'USD', 'EUR']
+    assert check_currency_codes(conversion_lst, lst_currencies) == ('USD', 'EUR')
+
+
+@pytest.mark.parametrize('conv_lst, expected_error', [(['100', 'USD', 'USD'], ValueError),
+                                                      (['10', 'USD', 'KZT'], ValueError),
+                                                      (['5', 'RUB', 'KZT'], ValueError),
+
+])
+def test_check_currency_codes_invalid(conv_lst, expected_error):
+    with pytest.raises(expected_error):
+        convert_value(api_data, conv_lst, lst_currencies)
+
